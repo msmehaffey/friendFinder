212d92a08031b793ea8493eae1f073a36e488a07
Shuler <msmehaf2@gmail.com>
2019-01-17 18:21:07 -0600

    Algorithim complete. Now calculates friend with the closest score. Still left to do -- host on heroku && some css for returning the friend return and form validation.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,38 @@
 var friendsArray = [
     {
-        userName: "test",
-        userPicture: "test",
+        userName: "test1",
+        userPicture: "test1",
         q1Answer: 1,
         q2Answer: 1,
         q3Answer: 1,
         q4Answer: 1,
         q5Answer: 1,
         total: function() {
-            var totalAnswers = this.q1Answer + this.q2Answer + this.q3Answer + this.q4Answer + this.q5Answer;
-            return totalAnswers;
+            return this.q1Answer +this.q2Answer + this.q3Answer + this.q4Answer + this.q5Answer;
+        }
+    },
+    {
+        userName: "test2",
+        userPicture: "test2",
+        q1Answer: 2,
+        q2Answer: 2,
+        q3Answer: 2,
+        q4Answer: 2,
+        q5Answer: 2,
+        total: function() {
+            return this.q1Answer +this.q2Answer + this.q3Answer + this.q4Answer + this.q5Answer;
+        }
+    },
+    {
+        userName: "test3",
+        userPicture: "test3",
+        q2Answer: 4,
+        q1Answer: 4,
+        q3Answer: 4,
+        q4Answer: 4,
+        q5Answer: 4,
+        total: function() {
+            return this.q1Answer +this.q2Answer + this.q3Answer + this.q4Answer + this.q5Answer;
         }
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,51 @@ module.exports = function(app) {
     }); 
 
     app.post("/api/friends", function(req, res) {
+        
+        //Pushing an array of each existing friends answers into an array called scoreArr
+        var scoreArr = []
+        for (i = 0; i < friendsData.length; i++) {
+            scoreArr.push([parseFloat(friendsData[i].q1Answer), parseFloat(friendsData[i].q2Answer), parseFloat(friendsData[i].q3Answer), parseFloat(friendsData[i].q4Answer), parseFloat(friendsData[i].q5Answer)])
+        };
+
+        //creating an array of the users answers called entrantScore
+        var entrantScore = [parseFloat(req.body.q1Answer), parseFloat(req.body.q2Answer), parseFloat(req.body.q3Answer), parseFloat(req.body.q4Answer), parseFloat(req.body.q5Answer)];
+
+        //
+        var diffValues = []
+
+        for (i = 0; i < scoreArr.length; i++) {
+            var individualDiff = []
+            for (j = 0; j < entrantScore.length; j++){
+                individualDiff.push(Math.abs(scoreArr[i][j] - entrantScore[j]));
+            };
+            function reducer(total, num) {
+                return total + num
+            };
+            diffValues.push(individualDiff.reduce(reducer));
+            individualDiff = [];
+        };
+
+
+        var index = 0;
+        var value = diffValues[0];
+        for (k = 0; k < diffValues.length; k++) {
+            if (diffValues[k] < value) {
+                index = k
+                value = diffValues[k]
+            }
+        };
+
+        console.log(value);
+        console.log(index);
+
+        console.log("Wahooo! Your friend match is.. " + friendsData[index].userName )
+
+
+
+
+        console.log(diffValues);
+        console.log(entrantScore);
         friendsData.push(req.body);
         res.json(true);
 
