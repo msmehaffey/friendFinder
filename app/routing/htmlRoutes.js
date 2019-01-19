f532cf14dca7e83df3d0044838c4dca75a3dd716
Shuler <msmehaf2@gmail.com>
2019-01-19 13:18:24 -0600

    added express middleware to pass static files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,5 +1,7 @@
 var path = require("path");
 
+
+
 module.exports = function(app) {
 
     app.get("/survey", function(req, res) {
--- app/server.js
+++ app/server.js
@@ -6,6 +6,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 require("./routing/apiRoutes.js")(app);
 require("./routing/htmlRoutes")(app);
